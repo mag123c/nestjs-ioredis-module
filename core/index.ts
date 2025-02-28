793f63f84836a8ae53fd9952177e42ffa166ea8a
mag123c <diehreo@gmail.com>
2025-02-28 11:21:12 +0900

    add: package.json added types, exports

--- core/index.ts
+++ core/index.ts
@@ -1,2 +1,6 @@
 export * from './redis.module';
 export * from './redis.decorator';
+export * from './redis.interface';
+export * from './redis.providers';
+export * from './redis.constasnts';
+export * from './redis.factory';
